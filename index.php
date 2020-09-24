3a6f514db65026e10393a2140a07900369b1067a
Muhammad Najibullah <55823870+muhamnajib@users.noreply.github.com>
2020-09-24 10:42:54 +0700

    Fix

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Latihan PASS</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  
+  <!-- bagian header template -->
+  <header>
+    <div class="topnav"> <!-- awal header -->
+    <h1 class="judul">Cloud Computing</h1>
+    </div> <!-- akhir header -->
+  </header>
+  <!-- akhir bagian header template -->
+  
+  <div class="wrap">
+    <!-- bagian sidebar website -->
+    <aside class="sidebar">
+      <div class="widget">
+        <h2>About Me</h2>
+        <img src="me.jpg" style="width: 200px;height: 200px;">
+        <h3>Muhammad Najibullah</h3>
+        <p>Saya merupakan mahasiswa semester 5 dari Universitas Islam Negeri Syarif Hidayatullah Jakarta dengan program studi Teknik Informatika.</p>
+      </div>
+    <!--   <div class="widget">
+        <h2>Contact Us</h2>
+        <p>Teman-teman bisa mendapatkan ebook tutorial gratis di sini <a target="_blank" href="https://www.malasngoding.com">www.malasngoding.com</a>.</p>
+      </div> -->
+    </aside>
+    <!-- akhir bagian sidebar website -->
+ 
+    <!-- bagian konten Blog -->
+    <div class="blog">
+      <div class="conteudo">
+        <div class="post-info">
+          Di Posting Oleh <b>Muhammad Najibullah</b>
+        </div>
+        <img class="foto"src="plt.jpg">
+        <h1> Rumah Sakit Syarif Hidayatullah</h1>
+        <hr>
+        <p>
+          Rumah Sakit Syarif Hidayatullah adalah rumah sakit swasta yang telah melayani masyarakat sejak tahun 1961. Berawal dari sebuah klinik kecil di lingkungan UIN (IAIN) Jakarta yang kemudian berkembang menjadi Rumah Sakit pada tahun 2007. Lokasi Rumah Sakit Syarif Hidayatullah sangat strategis, yaitu terletak di Jl. Ir. H. Djuanda No. 95, Pisangan, Ciputat Timur - Tangerang Selatan, Indonesia.Dengan adanya rumah sakit yang berada tepat di depan universitas akan memudahkan kita jika terjadi hal-hal yang tidak diinginkan maupun keadaan darurat seperti 
+          mahasiswa UIN yang terkena musibah kecelakaan ataupun ada yang ingin sekedar cek kesehatan.
+        </p>        
+      </div>
+      <div class="conteudo">
+        <div class="post-info">
+          Di Posting Oleh <b>Muhammad Najibullah</b>
+        </div>
+        <img src="wfh.png">
+        <h1> Study From Home </h1>
+        <hr>
+        <p>
+          Ada beberapa kegiatan menarik yang saya biasa dilakukan saat kondisi Work From Home atau Study From Home seperti ini dan bisa kalian coba juga:
+        </p>
+        <h3>1. Mencoba memasak resep makanan yang populer di media sosial</h3>
+        <img src="masak.jpg">
+        <p>Kegiatan ini selain bermanfaat untuk menambah skill memasakmu, juga dianggap sangat menyenangkan loh. Apalagi jika ternyata hasil masakanmu dipuji oleh orang rumah, karena cita rasanya yang menggugah selera.
+        Bila sebelumnya kamu hanya mempelajari cara memasak nasi goreng ataupun tumis sayur. Sekarang manfaatkan momen #dirumahaja dengan mempelajari resep baru yang banyak beredar di TikTok. Beberapa diantaranya seperti pie susu teflon, churros, roti goreng cokelat, dan dessert box oreo.</p>
+        <h3>2. Menonton film favorit atau drama yang sedang populer</h3>
+        <img src="film.png">
+        <p>Kamu bisa memanfaatkan layanan streaming film, mulai dari Netflix, Viu, hingga HOOQ. Salah satu kegiatan asyik yang bikin WFH jadi menyenangkan ini, bisa kamu lakukan di sela waktu istirahat siang ataupun saat tugas kuliah telah diselesaikan. Agar terkesan seperti sedang berada di bioskop, siapkan kudapan ringan untuk menemani aktivitasmu yang satu ini.</p>
+        <h3>3. Berolahraga ringan</h3>
+        <img src="olahraga.jpeg">
+        <p>Kegiatan asyik yang bikin WFH jadi menyenangkan selanjutnya adalah dengan melakukan olahraga ringan. Selain menyehatkan, kegiatan ini juga sangat dianjurkan untuk kamu yang ingin memiliki bentuk tubuh ideal. Kegiatan ini membuat tubuh tetap bugar karena kurangnya aktivitas saat WFH seperti ini bisa membuat tubuh kita mudah lelah karena jarang bergerak atau aktivitas seperti hari-hari normal.</p>
+        <h3>4. Bermain Game</h3>
+        <img src="game.jpg">
+        <p>Kegiatan asyik yang bikin WFH jadi menyenangkan selanjutnya adalah dengan bermain game offline maupun online. Kegiatan ini sangat sangat sering saya lakukan saat liburan antara semester, karena bermain game online bersama beberapa teman dapat sangat menyenangkan dan sejenak melepaskan penat yang ada karena #dirumahaja. Kegiatan ini boleh dilakukan saat sedang WFH atau Study From Home dengan syarat pekerjaan yang harus diselesaikan sudah terlaksana agar tidak mengganggu pekerjaan atau perkuliahan. Secara keseluruhan kegiatan bermain game ini tentunya sangat menyenangkan untuk dilakukan di masa WFH ini.</p>
+      </div>
+    </div>
+    <!-- akhir bagian konten Blog -->
+  </div>
+ 
+</body>
+</html>
